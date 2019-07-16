b94359c44ccd2f9d63ef8a4a50795846513ff352
jhonnrock <jhonn7890@gmail.com>
2019-07-15 20:36:17 -0400

    add driver and modify of chrome

--- src/main/java/testingui/diplomadoumss/org/core/Chrome.java
+++ src/main/java/testingui/diplomadoumss/org/core/Chrome.java
@@ -2,6 +2,7 @@ package testingui.diplomadoumss.org.core;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 /**
  * Implement the webdriver
@@ -10,6 +11,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Chrome implements Browser {
     @Override
     public WebDriver getWebDriver() {
-        return new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+
+        return new ChromeDriver(options);
+
     }
 }
